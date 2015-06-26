e53c51e606cb662c876f8484794fe7fdf04c612a
Tomas Cejka <cejkat@cesnet.cz>
2015-06-26 20:22:44 +0200

    travis-ci: add libjson-c

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,6 +3,8 @@
 if which apt-get >/dev/null 2>/dev/null; then
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y git libjson-c-dev libapr1-dev libaprutil1-dev libxml2-dev apache2-dev pkg-config libtool cmake libtool cmake libxslt1-dev libcurl4-openssl-dev
+wget http://security.ubuntu.com/ubuntu/pool/main/j/json-c/libjson-c-dev_0.11-3ubuntu1.2_amd64.deb http://security.ubuntu.com/ubuntu/pool/main/j/json-c/libjson-c2_0.11-3ubuntu1.2_amd64.deb
+sudo dpkg -i ./libjson-c-dev_0.11-3ubuntu1.2_amd64.deb libjson-c2_0.11-3ubuntu1.2_amd64.deb
 else
 sudo yum install -y git json-c-devel apr-devel apr-util-devel libxml2-devel httpd-devel pkg-config libtool cmake libxslt-devel curl-devel
 fi
